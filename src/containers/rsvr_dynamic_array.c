4ca0f48bbc2ec7414b14d5a632ac7a25df710bfb
jagger-harris <jagger.lee.harris@gmail.com>
2024-06-17 19:14:27 -0500

    Renamed needless dynamic_array to array

--- src/containers/rsvr_dynamic_array.c
+++ src/containers/rsvr_dynamic_array.c
@@ -5,56 +5,56 @@
 
 rsvr_dynamic_array_t rsvr_dynamic_array_create(unsigned int array_size,
                                                unsigned int element_size) {
-  rsvr_dynamic_array_t dynamic_array;
+  rsvr_dynamic_array_t array;
 
-  dynamic_array.data = malloc(array_size * element_size);
-  dynamic_array.amount = 0;
-  dynamic_array.capacity = array_size;
-  dynamic_array.element_size = element_size;
+  array.data = malloc(array_size * element_size);
+  array.amount = 0;
+  array.capacity = array_size;
+  array.element_size = element_size;
 
-  return dynamic_array;
+  return array;
 }
 
-void rsvr_dynamic_array_destroy(rsvr_dynamic_array_t* dynamic_array) {
-  free(dynamic_array->data);
-  dynamic_array->data = NULL;
-  dynamic_array->amount = 0;
-  dynamic_array->capacity = 0;
-  dynamic_array->element_size = 0;
+void rsvr_dynamic_array_destroy(rsvr_dynamic_array_t* array) {
+  free(array->data);
+  array->data = NULL;
+  array->amount = 0;
+  array->capacity = 0;
+  array->element_size = 0;
 }
 
-void* rsvr_dynamic_array_get(rsvr_dynamic_array_t* dynamic_array,
+void* rsvr_dynamic_array_get(rsvr_dynamic_array_t* array,
                              unsigned int index) {
-  if (index >= dynamic_array->amount) {
+  if (index >= array->amount) {
     return NULL;
   }
 
-  return (void*)((unsigned char*)dynamic_array->data +
-                 index * dynamic_array->element_size);
+  return (void*)((unsigned char*)array->data +
+                 index * array->element_size);
 }
 
-void rsvr_dynamic_array_push(rsvr_dynamic_array_t* dynamic_array,
+void rsvr_dynamic_array_push(rsvr_dynamic_array_t* array,
                              const void* element) {
   unsigned char* destination;
 
-  if (dynamic_array->amount >= dynamic_array->capacity) {
-    dynamic_array->capacity = (size_t)ceil(dynamic_array->capacity *
+  if (array->amount >= array->capacity) {
+    array->capacity = (size_t)ceil(array->capacity *
                                            RSVR_DYNAMIC_ARRAY_GROWTH_AMOUNT);
-    dynamic_array->data =
-        realloc(dynamic_array->data,
-                dynamic_array->capacity * dynamic_array->element_size);
+    array->data =
+        realloc(array->data,
+                array->capacity * array->element_size);
   }
 
-  destination = (unsigned char*)dynamic_array->data +
-                dynamic_array->amount * dynamic_array->element_size;
-  memcpy((void*)destination, element, dynamic_array->element_size);
-  dynamic_array->amount++;
+  destination = (unsigned char*)array->data +
+                array->amount * array->element_size;
+  memcpy((void*)destination, element, array->element_size);
+  array->amount++;
 }
 
-void rsvr_dynamic_array_pop(rsvr_dynamic_array_t* dynamic_array) {
-  free(dynamic_array->data);
-  dynamic_array->data = NULL;
-  dynamic_array->amount = 0;
-  dynamic_array->capacity = 0;
-  dynamic_array->element_size = 0;
+void rsvr_dynamic_array_pop(rsvr_dynamic_array_t* array) {
+  free(array->data);
+  array->data = NULL;
+  array->amount = 0;
+  array->capacity = 0;
+  array->element_size = 0;
 }
