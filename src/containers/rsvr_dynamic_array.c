b1669eac25a4bbc7b9ecb4c310c4d8e6af2cba54
jagger-harris <jagger.lee.harris@gmail.com>
2024-06-17 19:19:09 -0500

    Added dynamic array pop

--- src/containers/rsvr_dynamic_array.c
+++ src/containers/rsvr_dynamic_array.c
@@ -23,38 +23,39 @@ void rsvr_dynamic_array_destroy(rsvr_dynamic_array_t* array) {
   array->element_size = 0;
 }
 
-void* rsvr_dynamic_array_get(rsvr_dynamic_array_t* array,
-                             unsigned int index) {
+void* rsvr_dynamic_array_get(rsvr_dynamic_array_t* array, unsigned int index) {
   if (index >= array->amount) {
     return NULL;
   }
 
-  return (void*)((unsigned char*)array->data +
-                 index * array->element_size);
+  return (void*)((unsigned char*)array->data + index * array->element_size);
 }
 
-void rsvr_dynamic_array_push(rsvr_dynamic_array_t* array,
-                             const void* element) {
+void rsvr_dynamic_array_push(rsvr_dynamic_array_t* array, const void* element) {
   unsigned char* destination;
 
   if (array->amount >= array->capacity) {
-    array->capacity = (size_t)ceil(array->capacity *
-                                           RSVR_DYNAMIC_ARRAY_GROWTH_AMOUNT);
-    array->data =
-        realloc(array->data,
-                array->capacity * array->element_size);
+    array->capacity =
+        (size_t)ceil(array->capacity * RSVR_DYNAMIC_ARRAY_GROWTH_AMOUNT);
+    array->data = realloc(array->data, array->capacity * array->element_size);
   }
 
-  destination = (unsigned char*)array->data +
-                array->amount * array->element_size;
+  destination =
+      (unsigned char*)array->data + array->amount * array->element_size;
   memcpy((void*)destination, element, array->element_size);
   array->amount++;
 }
 
 void rsvr_dynamic_array_pop(rsvr_dynamic_array_t* array) {
-  free(array->data);
-  array->data = NULL;
-  array->amount = 0;
-  array->capacity = 0;
-  array->element_size = 0;
+  if (array->amount == 0) {
+    return;
+  }
+
+  array->amount--;
+
+  /* Shrink array if number of elements is less than a quarter */
+  if (array->amount < array->capacity / 4) {
+    array->capacity /= 2;
+    array->data = realloc(array->data, array->capacity * array->element_size);
+  }
 }
--- test/test_dynamic_array.c
+++ test/test_dynamic_array.c
@@ -28,10 +28,20 @@ void test_dynamic_array(void) {
   test_int = 3;
   rsvr_dynamic_array_push(&array, &test_int);
   assert(array.amount == 3);
-  assert(array.capacity == (unsigned int)(ceil(2 * RSVR_DYNAMIC_ARRAY_GROWTH_AMOUNT)));
+  assert(array.capacity ==
+         (unsigned int)(ceil(2 * RSVR_DYNAMIC_ARRAY_GROWTH_AMOUNT)));
 
   /* Test: Get value from array */
   assert(*(int*)rsvr_dynamic_array_get(&array, 2) == 3);
+
+  /* Test: Pop */
+  rsvr_dynamic_array_pop(&array);
+  assert(array.amount == 2);
+
+  /* Test: Ensure last element was removed */
+  assert(*(int*)rsvr_dynamic_array_get(&array, 0) == 1);
+  assert(*(int*)rsvr_dynamic_array_get(&array, 1) == 2);
+  assert(rsvr_dynamic_array_get(&array, 2) == NULL);
 
   rsvr_dynamic_array_destroy(&array);
   printf("All dynamic array tests passed!\n");
