86e7d501be61e11079a428f6d9ecaa5a6a8ac715
jagger-harris <jagger.lee.harris@gmail.com>
2024-10-08 16:34:37 -0500

    Removed floor and ceil functions

--- include/rsv_math.h
+++ include/rsv_math.h
@@ -1,6 +1,6 @@
 /*
   rsv_math.h
-  Contains math related constants and functions
+  Contains math related constants
 
   C Reservoir Library
   MIT License - https://choosealicense.com/licenses/mit/
@@ -12,43 +12,6 @@
 #ifndef RSV_MATH_H
 #define RSV_MATH_H
 
-#define RSV_PI 3.14159265358979323846
 #define RSV_GOLDEN_RATIO 1.61803398874989484820
-
-/**
- * @brief Get the largest integral value not greater than the argument
- *
- * @param x Integral value
- * @return int Smallest integral value not more than x
- */
-static int rsv_floor(float x);
-
-/**
- * @brief Get the smallest integral value not less than the argument
- *
- * @param x Integral value
- * @return int Smallest integral value not less than x
- */
-static int rsv_ceil(float x);
-
-/**************** Implementations ****************/
-
-static int rsv_floor(float x) {
-  if (x >= 0) {
-    return (int)x;
-  } else {
-    int int_part = (int)x;
-    return x == int_part ? x : int_part - 1;
-  }
-}
-
-static int rsv_ceil(float x) {
-  if (x >= 0) {
-    int int_part = (int)x;
-    return x == int_part ? x : (int_part + 1);
-  } else {
-    return (int)x;
-  }
-}
 
 #endif /* RSV_MATH_H */
